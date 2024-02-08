4300cfea73c6607697713388d73bb595609ef930
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2024-02-08 20:52:22 +0100

    Implemented serviceEntry management features
    - Added 'addService.html' template for new service entries
    - Created 'editService.html' template for modifying existing service entry
    - Created 'services.html' to display current service list
    - Developed 'ServiceEntryController.java' for serviceEntry request handling

--- src/main/java/com/supecars/cardealermanagement/controller/ServiceEntryController.java
+++ src/main/java/com/supecars/cardealermanagement/controller/ServiceEntryController.java
@@ -1,54 +1,72 @@
 package com.supecars.cardealermanagement.controller;
 
+import com.supecars.cardealermanagement.model.Car;
 import com.supecars.cardealermanagement.model.ServiceEntry;
+import com.supecars.cardealermanagement.service.CarService;
 import com.supecars.cardealermanagement.service.ServiceEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/services")
+@Controller
 public class ServiceEntryController {
 
     private final ServiceEntryService serviceEntryService;
 
+    private final CarService carService;
+
     @Autowired
-    public ServiceEntryController(ServiceEntryService serviceEntryService) {
+    public ServiceEntryController(ServiceEntryService serviceEntryService, CarService carService) {
         this.serviceEntryService = serviceEntryService;
+        this.carService = carService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<ServiceEntry>> getAllServices() {
+    @GetMapping("/services")
+    public String getAllServices(Model model) {
         List<ServiceEntry> services = serviceEntryService.getAllRegisteredServices();
-        return ResponseEntity.ok(services);
+        model.addAttribute("services", services);
+        return "services";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ServiceEntry> getServiceEntryById(@PathVariable int id) {
-        return serviceEntryService.getServiceEntryById(id)
-                .map(ResponseEntity::ok)
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @GetMapping("/addService")
+    public String addServiceEntryForm(Model model) {
+        List<Car> cars = carService.getAllCars();
+        model.addAttribute("cars", cars);
+        model.addAttribute("serviceEntry", new ServiceEntry());
+        return "addService";
     }
 
-    @PostMapping
-    public ResponseEntity<ServiceEntry> addServiceEntry(@RequestBody ServiceEntry serviceEntry) {
+    @PostMapping("/services")
+    public String addServiceEntry(@ModelAttribute ServiceEntry serviceEntry) {
         serviceEntryService.addNewServiceEntry(serviceEntry);
-        return new ResponseEntity<>(serviceEntry, HttpStatus.CREATED);
+        return "redirect:/services";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ServiceEntry> deleteServiceEntry(@PathVariable int id) {
+    @PostMapping("/services/delete/{id}")
+    public String deleteServiceEntry(@PathVariable int id) {
         serviceEntryService.deleteServiceEntry(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return "redirect:/services";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ServiceEntry> updateServiceEntry(@PathVariable int id, @RequestBody ServiceEntry serviceEntry) {
+    @PostMapping("/update_service/{id}")
+    public String updateServiceEntry(@PathVariable int id, @ModelAttribute ServiceEntry serviceEntry) {
         serviceEntryService.updateServiceEntry(id, serviceEntry);
-        return ResponseEntity.ok(serviceEntry);
+        return "redirect:/services";
+    }
+
+    @GetMapping("/editService/{id}")
+    public String editServiceEntryForm(@PathVariable int id, Model model){
+        ServiceEntry serviceEntry = serviceEntryService.getServiceEntryById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid inventory Id:" + id));
+        List<Car> cars = carService.getAllCars();
+        model.addAttribute("cars", cars);
+        model.addAttribute("serviceEntry", serviceEntry);
+        return "editService";
     }
 
 }
